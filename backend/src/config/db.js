7aa32e12cd0c0f2c0183545092bd97134f6a3911
Enma Reyes <enmareco.co@gmail.com>
2025-11-13 22:10:11 -0300

    feat: refactor configuración de Sequelize para soportar entornos de producción y local

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,15 +3,34 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST,
-    dialect: process.env.DB_DIALECT,
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  // 🟢 PRODUCCIÓN (Supabase)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
     logging: false,
-  }
-);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+    },
+  });
+} else {
+  // 🟡 LOCAL (PGAdmin)
+  sequelize = new Sequelize(
+    process.env.POSTGRES_DATABASE,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    {
+      host: process.env.POSTGRES_HOST,
+      dialect: process.env.DB_DIALECT,
+      logging: false,
+    }
+  );
+}
 
 export default sequelize;
+
