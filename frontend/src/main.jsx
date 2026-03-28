6289cd6abb1908e8eab82b05a102f354ae4e566a
Enma Reyes <enmareco.co@gmail.com>
2026-03-28 13:57:16 -0300

    feat: reorganizar el componente GoogleLoginButton y mover GoogleOAuthProvider a main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { CurrencyProvider } from "./context/CurrencyProvider";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CurrencyProvider>
-        <App />
-      </CurrencyProvider>
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <CurrencyProvider>
+          <App />
+        </CurrencyProvider>
+      </GoogleOAuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
